ad9fad12b73ff522b9d2c1fed23c0fb068925297
Tony Sims <zocle@zocle.net>
2020-04-11 21:25:06 -0500

    Update pathfind.c

--- pathfind.c
+++ pathfind.c
@@ -3,13 +3,8 @@
 
 char *pathfind(char *cmd)
 {
-<<<<<<< HEAD
 	char *pathvar = getenv("PATH");
 	char pd[64], *pathd;
-=======
-	char *pathvar = getenv("PWD");
- 	char pd[64], *pathd;
->>>>>>> 1576705be348f68f86c5d6068a44bcdaf2b5ee0d
 	char *ret = cmd;
 
 	pathd = strtok(pathvar, ":");
