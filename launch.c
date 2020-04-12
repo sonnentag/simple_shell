149847456ade45ac5e77cf617ebdaa68e1b223a4
shontrell <shontrell.scott3@gmail.com>
2020-04-12 23:27:05 +0000

    adding prototype to header and fix error

--- launch.c
+++ launch.c
@@ -7,37 +7,24 @@ int launch(char **argv)
 	char *cmd, *destdir;
 	char wd[1024];
 
-	if (strcmp(argv[0], "cd") == 0)
+	pid = fork();
+	if (pid == 0)
 	{
-		destdir = argv[1];
-		rc = chdir(destdir);
-
-		if (rc)
-			perror("chdir failed");
+		if (strchr(argv[0], 47) == NULL)
+			cmd = pathfind(argv[0]);
 		else
-
-			setenv("PWD", destdir, 1);
+			cmd = argv[0];
+		if (execvp(cmd, argv) == -1)
+			printf("%s: Command not found.\n", cmd);
+		exit(EXIT_FAILURE);
 	}
+	else if (pid < 0)
+		perror("hsh");
 	else
-	{
-		pid = fork();
-		if (pid == 0)
+		do
 		{
-			if (strchr(argv[0], 47) == NULL)
-				cmd = pathfind(argv[0]);
-			else
-				cmd = argv[0];
-			if (execvp(cmd, argv) == -1)
-				printf("%s: Command not found.\n", cmd);
-			exit(EXIT_FAILURE);
+			pid = waitpid(pid, &status, WUNTRACED);
 		}
-		else if (pid < 0)
-			perror("hsh");
-		else
-			do {
-				pid = waitpid(pid, &status, WUNTRACED);
-			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
-
+		while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	return (1);
 }
