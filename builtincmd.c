518983499c976071d23bcebcc1c68e43e5717275
shontrell <shontrell.scott3@gmail.com>
2020-04-13 01:03:49 +0000

    added and removed lines

--- builtincmd.c
+++ builtincmd.c
@@ -5,17 +5,18 @@
  */
 int builtincmd(char **argv)
 {
-	int x, rc;
+	int x, rc, ret = 0;
 	char *Builtinarray[] = {"exit", "cd", "env"}, *destdir;
 
 	for (x = 0; x < 3; x++)
 		if (strcmp(argv[0], Builtinarray[x]) == 0)
-		    break;
-
+		{
 		    switch (x)
 		    {
 		    case 0:
 			    exit(0);
+			    ret = 1;
+			    break;
 		    case 1:
 			    destdir = argv[1];
 			    rc = chdir(destdir);
@@ -23,11 +24,14 @@ int builtincmd(char **argv)
 				    perror("chdir failed");
 			    else
 				    setenv("PWD", destdir, 1);
+			    ret = 1;
+			    break;
 		    case 2:
 			    while (*environ)
 				    printf("%s\n", *environ++);
-		    default:
-			    return (1);
+			    ret = 1;
+			    break;
 		    }
-		    return (0);
+		}
+		    return (ret);
 }
