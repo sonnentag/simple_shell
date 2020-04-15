8c04776f48086049e322f9e65eb8e5ab99f41fca
Tony Sims <zocle@zocle.net>
2020-04-15 18:19:19 -0500

    added free for argv and *argv within loop

--- launch.c
+++ launch.c
@@ -22,6 +22,7 @@ int launch(char **argv)
 			cmd = argv[0];
 		if (execve(cmd, argv, envp) == -1)
 			printf("%s: Command not found.\n", cmd);
+		free(*argv);
 		free(argv);
 		exit(EXIT_FAILURE);
 	}
@@ -29,6 +30,8 @@ int launch(char **argv)
 		do {
 			pid = waitpid(pid, &status, WUNTRACED);
 		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+
+	free(*argv);
 	free(argv);
 
 	return (0);
