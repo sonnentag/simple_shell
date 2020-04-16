93b76a8b52b7cf8a2055cda0c37084410bb579c3
Tony Sims <zocle@zocle.net>
2020-04-16 14:45:45 -0500

    added getvar and memset with minor changes to existing code. Sorted hsh.h and removed unused _strlen proto

--- hsh.h
+++ hsh.h
@@ -9,20 +9,21 @@
 
 extern char **environ;
 
-char *read_line(void);
-char **split_line(char *line);
-int launch(char **args);
-char *pathfind(char *cmd);
-char *_strcat(char *dest, char *src);
-char *_strcpy(char *dest, char *src);
-char *_strdup(char *str);
 int builtincmd(char **argv);
 void sigintHandler(int sig);
-int _strcmp(char *s1, char *s2);
-int _strlen(char *s);
+char *getvar(char *vname);
+int launch(char **args);
+char *pathfind(char *cmd);
+char *read_line(void);
+char **split_line(char *line);
 
+char *_memset(char *s, char b, unsigned int n);
+int _strcmp(char *s1, char *s2);
 int _strcpos(char *path);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncatn(char *str, int n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
 
-#endif
+#endif /* _HSH_H */
