181e654f4a4364a2780a0615978dd4d9bca66cd3
shontrell <shontrell.scott3@gmail.com>
2020-04-16 14:58:52 +0000

    fixing betty errors

--- pathfind.c
+++ pathfind.c
@@ -1,6 +1,10 @@
 #include <sys/stat.h>
 #include "hsh.h"
-
+/**
+ * *pathfind - finds the PATH of a command passed
+ * @cmd: command
+ * Return: ret
+ */
 char *pathfind(char *cmd)
 {
 	char *pathvar = _strdup(getenv("PATH"));
@@ -13,13 +17,17 @@ char *pathfind(char *cmd)
 		_strcpy(pd, pathd);
 		strcat(pd, "/");
 		if (file_exist(strcat(pd, cmd)))
-		 	ret = _strdup(pd);
+			ret = _strdup(pd);
 
 		pathd = strtok(NULL, ":");
 	}
 	return (ret);
 }
-
+/**
+ * file_exist - checks if a file exist
+ * @filename: name of file
+ * Return: file status
+ */
 int file_exist(char *filename)
 {
 	struct stat buffer;
