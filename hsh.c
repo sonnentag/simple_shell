ddb0be40366f8babde22924a9a9a517ccbaeca09
shontrell <shontrell.scott3@gmail.com>
2020-04-16 14:58:21 +0000

    fixing betty errors

--- hsh.c
+++ hsh.c
@@ -3,11 +3,10 @@
 #include <string.h>
 #include <unistd.h>
 #include "hsh.h"
-
 /**
+ * main - Entry Point
  *
- *
- *
+ * Return: 0
  */
 int main(void)
 {
@@ -35,7 +34,10 @@ int main(void)
 	}
 	return (0);
 }
-
+/**
+ * sigintHandler - signal handler
+ * @sig: signal
+ */
 void sigintHandler(int sig)
 {
 	(void)sig;
